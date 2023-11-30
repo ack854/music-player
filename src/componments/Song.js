5731b695974b419f1718804466f3865d1c2b3075
EXPERIONGLOBAL\arun.ck <arun.ck@experionglobal.com>
2023-11-30 18:53:37 +0530

    image rotate

--- src/componments/Song.js
+++ src/componments/Song.js
@@ -3,7 +3,7 @@ import React from "react";
 const Song = ({ currentSong }) => { 
 	return ( 
 		<div className="song-container"> 
-			<img src={currentSong?.cover} alt={currentSong?.name}  style={{borderRadius:'50%'}}/> 
+			<img src={currentSong?.cover} alt={currentSong?.name}  style={{borderRadius:'50%', animation: `spin ${5}s linear infinite`}}/> 
 			<h2>{currentSong?.name}</h2> 
 			<h3>{currentSong?.artist}</h3> 
 		</div> 
